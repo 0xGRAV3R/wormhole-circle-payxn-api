4c845ab71481722b3d95101546bc1fee1f8a261c
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-10-30 16:39:40 -0700

    🌀 Updated transfer module

--- src/transfer/transfer.module.ts
+++ src/transfer/transfer.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TransferController } from './transfer.controller';
 import { TransferService } from './transfer.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [TransferController],
