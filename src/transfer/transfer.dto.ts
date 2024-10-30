530de4c66b60dddfa866844e2e04fae9e18e9330
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-10-30 16:37:22 -0700

    🌀 Udpated transfer dto

--- src/transfer/transfer.dto.ts
+++ src/transfer/transfer.dto.ts
@@ -1,11 +1,13 @@
-// src/transfer/transfer.dto.ts
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class TransferDto {
+  @ApiProperty({ description: 'The wallet address of the sender' })
   @IsString()
   @IsNotEmpty()
   wallet: string;
 
+  @ApiProperty({ description: 'The amount of USDC to transfer' })
   @IsNumber()
   @IsNotEmpty()
   amount: number;
