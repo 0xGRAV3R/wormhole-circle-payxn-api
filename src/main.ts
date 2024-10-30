489a4bcba50360447fb115480020217f47d5f384
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-10-30 16:32:53 -0700

    🌀 Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,16 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-
-  // Swagger setup
+  
   const config = new DocumentBuilder()
     .setTitle('Wormhole Circle PayXN API')
-    .setDescription('API for transferring USDC from Ethereum to Solana')
+    .setDescription('API for transferring USDC from Ethereum to Solana using Wormhole')
     .setVersion('1.0')
-    .addTag('transfer')
     .build();
-  
+    
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
